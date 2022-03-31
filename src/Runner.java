fdb25e2d9defa481bde336151a0c274952758b4d
Dhruv-Raman <71356377+Dhruv-Raman@users.noreply.github.com>
2022-03-31 11:14:11 -0700

    Runner with some code

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,8 @@
 public class Runner {
     public static void main(String[] args) {
-
+        GameSetup r1 = new GameSetup();
+        //System.out.println(r1.extractIndividualShip("A A1h B A6v D E3h S F4v P G7h"));
+        System.out.println(r1.setLocation("A A1h"));
     }
+    
 }
