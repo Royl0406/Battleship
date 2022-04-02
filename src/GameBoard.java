703ed0180c72dd51a0107f13da0038740f09732c
Royl0406 <royliuzy0406@gmail.com>
2022-04-02 11:08:24 -0700

    game grid created with buttons

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -2,15 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameBoard {
-    final int width = 500;
-    final int height = 500;
+    final int width = 1000;
+    final int height = 1000;
     JFrame frame;
     public GameBoard() {
         frame = new JFrame("Battleship");
         frame.setSize(width, height);
-        frame.getContentPane().setLayout(new FlowLayout());
+        frame.getContentPane().setLayout(new GridLayout(10, 10));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        
+        for(int i = 0; i < 100; i++) {
+            JButton button = new JButton();
+            button.setBorder(BorderFactory.createLineBorder(Color.yellow));
+            button.setBackground(Color.black);
+            button.setOpaque(true);
+            frame.add(button);
+        }
+
+        frame.setVisible(true);
     }
 }
