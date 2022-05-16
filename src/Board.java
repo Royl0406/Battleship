a37075adacf4e972975ef8616318242e8ce1cab3
Dhruv-Raman <71356377+Dhruv-Raman@users.noreply.github.com>
2022-05-16 12:19:19 -0700

    New board class yay

---
+++ src/Board.java
@@ -0,0 +1,13 @@
+public class Board{
+    private char[][] boardArray;
+    public Board(char[][] input){
+        boardArray = input;
+    }
+
+    public char[][] getBoard(){
+        return boardArray;
+    }
+
+    
+
+}
