2907209996a38b8cc4f4bef485fae6387d5b8b1e
Royl0406 <royliuzy0406@gmail.com>
2022-05-23 09:59:55 -0700

    Co-authored-by: Dhruv-Raman <Dhruv-Raman@users.noreply.github.com>

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -8,10 +8,13 @@ public class GameBoard {
     JPanel board;
 
     public GameBoard() {
+
+    }
+
+    public void showBoard() {
         frame = new JFrame("Battleship");
         frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
 
         board = new JPanel();
         board.setLayout(new GridLayout(11, 11));
@@ -38,59 +41,99 @@ public class GameBoard {
         ImageIcon I = new ImageIcon("Star Wars images/I.png");
         ImageIcon J = new ImageIcon("Star Wars images/J.png");
 
-        //adding labels for each column (1-10)
-        for(int i = 0; i < 11; i++) {                    
-            switch(i) {
-                case 1: board.add(new JButton(zero)); break;
-                case 2: board.add(new JButton(one)); break;
-                case 3: board.add(new JButton(two)); break;
-                case 4: board.add(new JButton(three)); break;
-                case 5: board.add(new JButton(four)); break;
-                case 6: board.add(new JButton(five)); break;
-                case 7: board.add(new JButton(six)); break;
-                case 8: board.add(new JButton(sev)); break;
-                case 9:board.add(new JButton(eight)); break;
-                case 10: board.add(new JButton(nine)); break;
+        // adding labels for each column (1-10)
+        for (int i = 0; i < 11; i++) {
+            switch (i) {
+                case 1:
+                    board.add(new JButton(zero));
+                    break;
+                case 2:
+                    board.add(new JButton(one));
+                    break;
+                case 3:
+                    board.add(new JButton(two));
+                    break;
+                case 4:
+                    board.add(new JButton(three));
+                    break;
+                case 5:
+                    board.add(new JButton(four));
+                    break;
+                case 6:
+                    board.add(new JButton(five));
+                    break;
+                case 7:
+                    board.add(new JButton(six));
+                    break;
+                case 8:
+                    board.add(new JButton(sev));
+                    break;
+                case 9:
+                    board.add(new JButton(eight));
+                    break;
+                case 10:
+                    board.add(new JButton(nine));
+                    break;
 
             }
         }
-    
 
-    for(int i = 0;i < 11;i++) {
-        for (int j = 0; j < 11; j++) {
-            if(i == 0 && j == 0) {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (i == 0 && j == 0) {
+                    JButton button = new JButton();
+                    button.setBorder(BorderFactory.createLineBorder(Color.yellow));
+                    button.setBackground(Color.black);
+                    button.setOpaque(true);
+                    board.add(button);
+                    break;
+                }
+                // adding labels for each rows (A-J)
+                if (i != 0 && j == 10) {
+                    switch (i) {
+                        case 1:
+                            board.add(new JButton(A));
+                            break;
+                        case 2:
+                            board.add(new JButton(B));
+                            break;
+                        case 3:
+                            board.add(new JButton(C));
+                            break;
+                        case 4:
+                            board.add(new JButton(D));
+                            break;
+                        case 5:
+                            board.add(new JButton(E));
+                            break;
+                        case 6:
+                            board.add(new JButton(F));
+                            break;
+                        case 7:
+                            board.add(new JButton(G));
+                            break;
+                        case 8:
+                            board.add(new JButton(H));
+                            break;
+                        case 9:
+                            board.add(new JButton(I));
+                            break;
+                        case 10:
+                            board.add(new JButton(J));
+                            break;
+
+                    }
+                    break;
+                }
                 JButton button = new JButton();
                 button.setBorder(BorderFactory.createLineBorder(Color.yellow));
                 button.setBackground(Color.black);
                 button.setOpaque(true);
                 board.add(button);
-                break;
             }
-            //adding labels for each rows (A-J)
-            if(i != 0 && j == 10) {
-                switch(i) {
-                    case 1: board.add(new JButton(A)); break;
-                    case 2: board.add(new JButton(B)); break;
-                    case 3: board.add(new JButton(C)); break;
-                    case 4: board.add(new JButton(D)); break;
-                    case 5: board.add(new JButton(E)); break;
-                    case 6: board.add(new JButton(F)); break;
-                    case 7: board.add(new JButton(G)); break;
-                    case 8: board.add(new JButton(H)); break;
-                    case 9:board.add(new JButton(I)); break;
-                    case 10: board.add(new JButton(J)); break;
-    
-                }
-                break;
-            }
-            JButton button = new JButton();
-            button.setBorder(BorderFactory.createLineBorder(Color.yellow));
-            button.setBackground(Color.black);
-            button.setOpaque(true);
-            board.add(button);
-        }
 
+        }
+        frame.add(board, BorderLayout.CENTER);
+        frame.setVisible(true);
     }
-    frame.add(board,BorderLayout.CENTER);
-    frame.setVisible(true);
-}}
+}
--- src/GameDriver.java
+++ src/GameDriver.java
@@ -4,10 +4,14 @@ public class GameDriver {
     
     private Player p1;
     private Player p2;
+    private GameBoard p1Board;
+    private GameBoard p2Board;
     
     public GameDriver() {
         p1 = new Player(0);
         p2 = new Player(1);
+        p1Board = new GameBoard();
+        p2Board = new GameBoard();
         
         }
     
@@ -20,6 +24,9 @@ public class GameDriver {
         while(!validName){
             validName = p1.setUsername();
         }
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
         
         System.out.println("Player 2: Please enter your username.");
         
@@ -28,8 +35,19 @@ public class GameDriver {
             validName = p2.setUsername();
         }
 
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
         p1.setShipLocations();
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+
         p2.setShipLocations();
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
 
         boolean gameOver = false;
 
@@ -50,6 +68,9 @@ public class GameDriver {
             p1.addShot();
             gameOver = p2.getBoard().gameState();
 
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
             System.out.println(p2.getName()+", choose your target.");
             
             
@@ -63,6 +84,9 @@ public class GameDriver {
             p2.addHit(valid);
             p2.addShot();
             gameOver = p1.getBoard().gameState();
+
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
 
 
         }
